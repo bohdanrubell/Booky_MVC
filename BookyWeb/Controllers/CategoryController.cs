5af9fba559b8461734bb99ea8896aadfd5923406
Bohdan <bohdanrubell@gmail.com>
2023-09-20 20:42:08 +0300

    Added notifications Added func like as Edit,Delete

--- BookyWeb/Controllers/CategoryController.cs
+++ BookyWeb/Controllers/CategoryController.cs
@@ -28,15 +28,70 @@ namespace BookyWeb.Controllers
             {
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
             }
-                if (ModelState.IsValid)
-                {
-                    _db.Categories.Add(obj);
-                    _db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-       
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
             return View();
-            
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Category? categoryFromBase = _db.Categories.Find(id);
+            if (categoryFromBase == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromBase);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Category? categoryFromBase = _db.Categories.Find(id);
+            if (categoryFromBase == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromBase);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
 
     }
