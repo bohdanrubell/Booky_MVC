1f4ace4b0859d8a380a1ac703aa34bae30deb1bc
Bohdan <bohdanrubell@gmail.com>
2023-09-20 22:24:47 +0300

    Add Razor Project(experemental)

---
+++ BookyWeb_Razor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using BookyWeb_Razor.Data;
+using BookyWeb_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookyWeb_Razor.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ BookyWeb_Razor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,35 @@
+using BookyWeb_Razor.Data;
+using BookyWeb_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookyWeb_Razor.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Category? category = _db.Categories.Find(Category.Id);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
