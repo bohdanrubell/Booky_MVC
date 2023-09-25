65cc3850e1f0d4f0464a9e5dd5c41d0396bca064
Bohdan <bohdanrubell@gmail.com>
2023-09-25 21:23:37 +0300

    ADDED CRUD FOR PRODUCT IMPORTATNT COMMIT!

---
+++ BookyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using Booky.DataAccess.Repository.IRepository;
+using Booky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objCategoryList = _unitOfWork.Product.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            //if (obj. == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            //}
+            _unitOfWork.Product.Add(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Product? productFromBase = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productFromBase == null)
+            {
+                return NotFound();
+            }
+            return View(productFromBase);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            //if (ModelState.IsValid)
+            //{
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            
+            //return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Product? productFromBase = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productFromBase == null)
+            {
+                return NotFound();
+            }
+            return View(productFromBase);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
