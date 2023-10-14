42ad8baab2eb794a8f968a506a50b2bd5526f100
Bohdan <bohdanrubell@gmail.com>
2023-10-14 21:52:16 +0300

    .NET7 Downgrade!!!!!

--- BookyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BookyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -89,7 +89,7 @@ namespace BookyWeb.Areas.Admin.Controllers
             orderHeader.ShippingDate = DateTime.Now;
             if(orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
             {
-                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+                orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
             }
 
             _unitOfWork.OrderHeader.Update(orderHeader);
